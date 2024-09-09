2f698163ef0b0db3416c205c691d0e6f237df85f
Nazariy Holovach <nazariyholovach@Nazariys-MacBook-Air.local>
2024-09-09 11:26:38 +0300

    Added navbar menu

--- rhythmo_vue/src/App.vue
+++ rhythmo_vue/src/App.vue
@@ -1,30 +1,77 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <Navbar />
   <router-view/>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import Navbar from './components/Navbar.vue'
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+export default {
+  components: {
+    Navbar,
   }
 }
+</script>
+
+<style lang="scss">
+/* Reset box-sizing */
+*,
+*::before,
+*::after {
+    box-sizing: border-box;
+}
+
+/* Reset margin and padding */
+body, h1, h2, h3, h4, h5, h6, p, ul, ol, li, button, input, textarea, select {
+    margin: 0;
+    padding: 0;
+}
+
+/* Remove list styles */
+ul, ol {
+    list-style: none;
+}
+
+/* Remove link underlines */
+a {
+    text-decoration: none;
+    color: inherit;
+}
+
+/* Reset button styles */
+button {
+    border: none;
+    background: none;
+    padding: 0;
+    font: inherit;
+    cursor: pointer;
+}
+
+/* Remove default button border and background */
+button:focus {
+    outline: none;
+}
+
+/* Reset input styles */
+input,
+textarea,
+select {
+    border: none;
+    padding: 0;
+    font: inherit;
+    color: inherit;
+    outline: none;
+    box-shadow: none;
+}
+
+/* Reset font family */
+body {
+    font-family: Arial, sans-serif;
+}
+
+/* Ensure all elements inherit font color */
+body, h1, h2, h3, h4, h5, h6, p, ul, ol, li, a, button, textarea, select {
+    color: inherit;
+}
+
 </style>
