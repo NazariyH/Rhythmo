81d8be3c16878e46ae734e74149993ba7023b5bf
Nazariy Holovach <nazariyholovach@Nazariys-MacBook-Air.local>
2024-09-21 18:00:02 +0300

    Added ability to reset image and see previous profile data in ProfileEditView

--- frontend/src/views/ProfileEditView.vue
+++ frontend/src/views/ProfileEditView.vue
@@ -8,8 +8,8 @@
                 </div>
 
                 <div>
-                    <input v-model="username" type="text" id="username" required>
-                    <input v-model="age" type="number" id="age" required>
+                    <input v-model="username" type="text" id="username">
+                    <input v-model="age" type="number" id="age">
                 </div>
             </div>
 
@@ -33,7 +33,7 @@
             <div class="vertical">
                 <button type="submit">Save</button>
 
-                <label for="image">
+                <label v-if="!profileImage" for="image">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                         class="bi bi-image" viewBox="0 0 16 16">
                         <path d="M6.002 5.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0" />
@@ -42,6 +42,15 @@
                     </svg>
                 </label>
 
+                <button v-else v-on:click="resetProfileImage" class="resetImage">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                        class="bi bi-image" viewBox="0 0 16 16">
+                        <path d="M6.002 5.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0" />
+                        <path
+                            d="M2.002 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2zm12 1a1 1 0 0 1 1 1v6.5l-3.777-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12V3a1 1 0 0 1 1-1z" />
+                    </svg> 
+                </button>
+
                 <input type="file" id="image" v-on:change="onFileChange">
             </div>
         </form>
@@ -49,53 +58,80 @@
 </template>
 
 <script>
-    import axios from 'axios'
+import axios from 'axios'
 
 
-    export default {
-        name: 'ProfileEditView',
-        data() {
-            return {
-                username: '',
-                age: null,
-                bio: '',
-                gender: null,
-                profileImage: '',
+export default {
+    name: 'ProfileEditView',
+    data() {
+        return {
+            username: '',
+            age: null,
+            bio: '',
+            gender: null,
+            profileImage: '',
+        }
+    },
+    methods: {
+        onFileChange(event) {
+            this.profileImage = event.target.files[0]
+        },
+
+        resetProfileImage() {
+            this.profileImage = ''
+        },
+
+        async fetchProfile() {
+            const token = localStorage.getItem('token')
+
+            if (!token) {
+                console.log('Token not found')
+                return
+            }
+
+            try {
+                const response = await axios.get('user/profile/')
+
+                this.username = response.data.profile.fullName
+                this.age = response.data.profile.age
+                this.bio = response.data.profile.bio
+                this.gender = response.data.profile.gender
+            } catch (error) {
+                console.log(error)
             }
         },
-        methods: {
-            onFileChange(event) {
-                this.profileImage = event.target.files[0]
-            },
 
-            async submitProfile() {
-                try {
-                    const token = localStorage.getItem('token')
+        async submitProfile() {
+            try {
+                const token = localStorage.getItem('token')
 
-                    const data = new FormData()
-                    data.append('fullName', this.username)
-                    data.append('age', this.age)
-                    data.append('bio', this.bio)
-                    data.append('gender', this.gender)
-                    data.append('profileImage', this.profileImage)
+                const data = new FormData()
+                data.append('fullName', this.username)
+                data.append('age', this.age)
+                data.append('bio', this.bio)
+                data.append('gender', this.gender)
+                data.append('profileImage', this.profileImage)
 
-                    const response = await axios.post('user/profile/', data, {
-                        headers: {
-                            'Authorization': `Token ${token}`,
-                            'Content-Type': 'multipart/form-data',                        
-                        }
-                    })
+                const response = await axios.post('user/profile/', data, {
+                    headers: {
+                        'Authorization': `Token ${token}`,
+                        'Content-Type': 'multipart/form-data',
+                    }
+                })
 
-                    console.log('Profile submitted successfully:', response.data)
-                } catch (error) {
-                    console.error('Error submitting profile:', error)
-                }
-            },
-        }
-    }
+                this.$router.push('/user/profile/')
+            } catch (error) {
+                console.error('Error submitting profile:', error)
+            }
+        },
+    },
+    mounted() {
+        this.fetchProfile()
+    },
+}
 </script>
 
 <style>
-    @import "@/assets/styles/profile.scss";
-    @import "@/assets/styles/authentication.scss";
+@import "@/assets/styles/profile.scss";
+@import "@/assets/styles/authentication.scss";
 </style>
